b2c5c11d227db44ee1f08598ed20cd6eef2bccde
Matthew Clifford <35276477+smilegodly@users.noreply.github.com>
2021-03-23 20:23:36 -0400

    Added youtube api request

---
+++ src/apis/youtube.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+let json = require("../key.json");
+
+const KEY = json.key;
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    type: "video",
+    maxResults: 5,
+    key: KEY,
+  },
+});
